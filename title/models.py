a682584670f9f2d558d52506d48f8cf1111ab0d0
Eray <solenkoleray@gmail.com>
2023-05-05 19:55:08 +0300

    Added Title and Entry models.
    Added Title and Entry models.

--- title/models.py
+++ title/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from account.models import User
 
 # Create your models here.
+
+class Title(models.Model):
+    title_name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title_text
+    
+
+class Entry(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    entry_text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.author.username + "- " + self.title.title_name
+    
+
+    
