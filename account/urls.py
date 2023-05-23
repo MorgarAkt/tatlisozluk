38675d24bfece56c38a51a62d8fbef73cedaf6a8
Eray <solenkoleray@gmail.com>
2023-05-23 15:48:36 +0300

    profile update page added, final fixes
    profile update page added, final fixes

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("login", views.login_request, name="login"),
     path("register", views.register_request, name="register"),
     path("logout", views.logout_request, name = "logout"),
-    path("user/<str:username>/", views.user_details, name = "user_details")
+    path("user/<str:username>/", views.user_details, name = "user_details"),
+    path("user/<str:username>/edit", views.edit_user, name = "edit_user"),
+    path("user/<str:username>/edit/done", views.change_user, name = "change_user"),
 ]
